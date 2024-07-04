013470300babf86c9fe2198b7fd7ed6c80cc25ad
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-04 12:05:24 -0300

    feat: Configura o projeto com tailwind

---
+++ next-crud/src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center bg-gradient-to-t from-purple-500 to-blue-900">
+        <span>
+          Hello mundo!
+        </span>
+      </div>
+    </>
+  );
+}
