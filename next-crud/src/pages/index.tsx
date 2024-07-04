bde68398a8d9961af995cf664ad1d762848b27d1
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-04 14:55:26 -0300

    feat: Inicia a construção do layout

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,10 +1,15 @@
+import Layout from "@/components/Layout";
+
 export default function Home() {
   return (
     <>
-      <div className="flex h-screen justify-center items-center bg-gradient-to-t from-purple-500 to-blue-900">
-        <span>
-          Hello mundo!
-        </span>
+      <div className="flex justify-center items-center h-screen
+      bg-gradient-to-t from-purple-500 to-blue-900
+      text-white
+      ">
+        <Layout titulo="Cadastro Simples">
+          <span>Conteudo!</span>
+        </Layout>
       </div>
     </>
   );
